76da2e544086a9bc27fc5bbb5d5e0271fb9e94b5
Bernardo <b3rnardogfontes@gmail.com>
2026-06-05 17:17:50 -0300

    feat(login): mudança de html para php

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,7 @@ namespace App\Controllers;
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
+//! Sempre que mexer no routes tem que dar autoload
+
 $router->get('', 'ExampleController@index');
+$router->get('login', 'LoginController@index');
