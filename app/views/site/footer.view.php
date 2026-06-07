40ee6dd13d5a8de485b9a00ffd2bedd429e51344
Camille Roberta de Almeida Torres <camilletorres876@gmail.com>
2026-06-07 10:46:16 -0300

    feat: salvando

--- app/views/site/footer.view.php
+++ app/views/site/footer.view.php
@@ -16,83 +16,89 @@
     <title>Footer</title>
 </head>
 
-<body>
-
-    <section class="logos">
-        <img class="polvoLogo" src="../../../public/assets/Logo.svg" alt="Logo polvo">
-        <img claa="nomeLogo" src="../../../public/assets/LOGO (1).svg" alt="Logo nome">
+<body class="corpoFooter">
+    <!-- <section class="containeronda">
+        <img src="../../../public/assets/onda.png" alt="onda" class="onda">
     </section>
 
-    <section class="sobrenos">
-        <h1 class="titulo corDestaque">SOBRE NÓS</h1>
+    <section class="informacoesfooter"> -->
 
-        <div class="conteudosobrenos">
-            <div class="trecho">
+        <section class="logos">
+            <img class="polvoLogo" src="../../../public/assets/Logo.svg" alt="Logo polvo">
+            <img claa="nomeLogo" src="../../../public/assets/LOGO (1).svg" alt="Logo nome">
+        </section>
 
-                <!-- span é tipo p, mas nn quebra a linha, mas eu coloquei span pq se colocasse outro p da problema pq tem p dentro de p sem fechar -->
+        <section class="sobrenos">
+            <h1 class="titulo corDestaque">SOBRE NÓS</h1>
 
-                <p class="conteudo par corParagrafos">
-                    <span class="subtitulo corDestaque">Missão: </span>
-                    Mergulhar na essência da música e
-                    desvendar o conceito da arte que existe por trás
-                    de cada nota, conectando o ouvinte à alma da obra.
-                </p>
+            <div class="conteudosobrenos">
+                <div class="trecho">
 
+                    <!-- span é tipo p, mas nn quebra a linha, mas eu coloquei span pq se colocasse outro p da problema pq tem p dentro de p sem fechar -->
+
+                    <p class="conteudo par corParagrafos">
+                        <span class="subtitulo corDestaque">Missão: </span>
+                        Mergulhar na essência da música e
+                        desvendar o conceito da arte que existe por trás
+                        de cada nota, conectando o ouvinte à alma da obra.
+                    </p>
+
+                </div>
+
+                <div class="trecho">
+
+                    <p class="conteudo par corParagrafos">
+                        <span class="subtitulo corDestaque">Visão: </span>
+                        Tornar-se o santuário digital para quem
+                        não apenas ouve, mas sente a música.
+                    </p>
+
+                </div>
+
+                <div class="trecho">
+
+                    <p class="conteudo par corParagrafos">
+                        <span class="subtitulo corDestaque">Valores: </span>
+                        Imersão cultural, respeito à identidade
+                        artística, profundidade técnica e a valorização do
+                        álbum como uma peça única de arte.
+                    </p>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="contatos">
+            <div class="redessociais">
+                <h2 class="redes corDestaque">REDES SOCIAIS</h2>
+
+                <div class="logosRedesSociais">
+
+                    <a href="https://www.linkedin.com/company/codejr">
+                        <img class="aumenta" src="../../../public/assets/Linkedin.svg" alt="Linkedin">
+                    </a>
+
+                    <a href="https://www.facebook.com/codeempresajunior">
+                        <img class="aumenta" src="../../../public/assets/Facebook.svg" alt="Facebook">
+                    </a>
+
+                    <a href="https://www.codejr.com.br/contact">
+                        <img class="aumenta" src="../../../public/assets/EnvelopeFill (2).svg" alt="Email">
+                    </a>
+
+                    <a href="https://www.instagram.com/codejr/">
+                        <img class="aumenta" src="../../../public/assets/Instagram.svg" alt="Instagram">
+                    </a>
+
+                </div>
             </div>
 
-            <div class="trecho">
-                
-                <p class="conteudo par corParagrafos">
-                    <span class="subtitulo corDestaque">Visão: </span>
-                    Tornar-se o santuário digital para quem
-                    não apenas ouve, mas sente a música.
-                </p>
-
+            <div class="codefooter">
+                <img src="../../../public/assets/Mask group.svg" alt="Logo Code">
+                <p class="des subtitulo corDestaque">Desenvolvido por: CodeJR</p>
             </div>
-
-            <div class="trecho">
-                
-                <p class="conteudo par corParagrafos">
-                    <span class="subtitulo corDestaque">Valores: </span>
-                    Imersão cultural, respeito à identidade
-                    artística, profundidade técnica e a valorização do
-                    álbum como uma peça única de arte.
-                </p>
-
-            </div>
-        </div>
-    </section>
-
-    <section class="contatos">
-        <div class="redessociais">
-            <h2 class="redes corDestaque">REDES SOCIAIS</h2>
-
-            <div class="logosRedesSociais">
-
-                <a href="https://www.linkedin.com/company/codejr">
-                    <img class="aumenta" src="../../../public/assets/Linkedin.svg" alt="Linkedin">
-                </a>
-
-                <a href="https://www.facebook.com/codeempresajunior">
-                    <img class="aumenta" src="../../../public/assets/Facebook.svg" alt="Facebook">
-                </a>
-
-                <a href="https://www.codejr.com.br/contact">
-                    <img class="aumenta" src="../../../public/assets/EnvelopeFill (2).svg" alt="Email">
-                </a>
-
-                <a href="https://www.instagram.com/codejr/">
-                    <img class="aumenta" src="../../../public/assets/Instagram.svg" alt="Instagram">
-                </a>
-
-            </div>
-        </div>
-
-        <div class="codefooter">
-            <img src="../../../public/assets/Mask group.svg" alt="Logo Code">
-            <p class="des subtitulo corDestaque">Desenvolvido por: CodeJR</p>
-        </div>
-    </section>
+        </section>
+    <!-- </section> -->
 
 </body>
 
