3002bd1e2bf40fb0fb755725cf064e5c9eda1621
Laura <lauraobastoos@gmail.com>
2026-06-10 17:46:56 -0300

    fix(landingpge): tranformação do arquivo de HTML para PHP mais alguns ajustes a pedido do cliente

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,6 @@ use App\Controllers\ExampleController;
 use App\Core\Router;
 
 $router->get('', 'ExampleController@index');
+
+// caminho para ir para landingpage
+$router->get('landingpage', 'landingpageController@index');
