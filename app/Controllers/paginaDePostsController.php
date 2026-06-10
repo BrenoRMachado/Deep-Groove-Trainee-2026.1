1a41d0a4d1a52622ddcd2707ca6cd388b4b00b3d
Luan Dutra <ldasilvadutradias@gmail.com>
2026-06-10 17:48:39 -0300

    feat(paginaDePosts): pagina recebe os dados dos posts, pagina atual e total de paginas

--- app/Controllers/paginaDePostsController.php
+++ app/Controllers/paginaDePostsController.php
@@ -8,6 +8,30 @@ use Exception;
 // Controller da página de posts:
 class PaginaDePostsController {
     public function index() {
-        return view('site/paginaDePosts');
+
+        $bancoDeDados = App::get('database');
+
+        $limite = 6;
+
+        $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+        if ($paginaAtual < 1){
+            $paginaAtual = 1;
+        }
+
+        $offset = ($paginaAtual - 1) * $limite;
+
+        $totalDePosts = $bancoDeDados -> countAllPosts('publicacoes');
+
+        $totalDePaginas = ceil($totalDePosts / $limite);
+
+        $posts = $bancoDeDados -> paginatePosts('publicacoes', $limite, $offset);
+
+        return view('site/paginaDePosts', [
+            'posts' => $posts,
+            'paginaAtual' => $paginaAtual,
+            'totalDePaginas' => $totalDePaginas
+        ]);
+
     }
+
 }
