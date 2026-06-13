7024d21e7cc9f8386a884f55ddb859c33e0e80ff
Camille Roberta de Almeida Torres <camilletorres876@gmail.com>
2026-06-13 16:09:17 -0300

    feat: salvando

---
+++ app/views/admin/dashboard.view.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Deep Groove</title>
+
+    <!-- *CSS -->
+    <link rel="stylesheet" href="../../../public/css/dashboard.css">
+
+    <!-- *FONTES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <!-- *ICONES -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"> 
+
+</head>
+<body>
+    <main id="dashboard">
+        <header id="cabecalho">
+            <h1 class="titulo-cabecalho">Dashboard</h1>
+            <div class="container-perfil">
+                <div class="container-dados-do-admin">
+                    <span class="dados-do-admin nome-admin">Nome do administrador</span>
+                    <span class="dados-do-admin cargo-admin">Administrador</span>
+                </div>
+                <button class="icone-perfil">
+                    <i class="bi bi-person-check"></i>
+                </button>
+            </div>
+        </header>
+        <nav id="area-navegacao-e-estatisticas">
+            <ul class="lista-area-navegacao-e-estatisticas">
+                <li class="container-usuarios-publicacoes-recentes container-usuarios">
+                    <div class="numero-de-usuarios-publicacoes-e-recente numero-de-usuarios">
+                        <i class="icone-usuarios-tabelas-e-recentes icone-usuarios bi bi-people-fill"></i>
+                        <span class="texto-dados-totais texto-dados-usuarios">
+                            Total de Usuários: 
+                            <span class="quantidade-total-de-usuarios"><?= $totalDeUsuarios ?></span>
+                        </span>
+                    </div>
+                    <button class="botao-navegar-para-paginas-administrativas botao-navegar-para-tabela-de-usuarios">
+                        Tabela de Usuários
+                        <i class="icone-navegar bi bi-chevron-right"></i>
+                    </button>
+                </li>
+                <li class="container-usuarios-publicacoes-recentes container-ultima-atividade">
+                    <div class="numero-de-usuarios-publicacoes-e-recente ultima-atividade">
+                        <i class="icone-usuarios-tabelas-e-recentes icone-ultima-atividade bi bi-activity"></i>
+                        <span class="texto-dados-totais texto-dados-ultima-atividade">
+                            Última atividade: 
+                        </span>
+                    </div>
+                    <div class="informacao-ultima-atividade informacao-recente">
+                        <span>Novo usuário cadastrado</span>
+                    </div>
+                </li>
+                <li class="container-usuarios-publicacoes-recentes container-ultima-publicacao">
+                    <div class="numero-de-usuarios-publicacoes-e-recente ultima-publicacao">
+                        <i class="icone-usuarios-tabelas-e-recentes icone-ultima-publicacao bi bi-images"></i>
+                        <span class="texto-dados-totais texto-dados-ultima-publicacao">
+                            Última publicação: 
+                        </span>
+                    </div>
+                    <div class="informacao-ultima-publicacao informacao-recente">
+                        <span>Publicação mais recente</span>
+                    </div>
+                </li>
+                <li class="container-usuarios-publicacoes-recentes container-publicacoes">
+                    <div class="numero-de-usuarios-publicacoes-e-recente numero-de-publicacoes">
+                        <i class="icone-usuarios-tabelas-e-recentes icone-publicacoes bi bi-disc"></i>
+                        <span class="texto-dados-totais texto-dados-publicacoes">
+                            Total de Publicações:
+                            <span class="quantidade-total-de-publicacoes"><?= $totalDePosts ?></span>
+                        </span>
+                    </div>
+                    <button class="botao-navegar-para-paginas-administrativas botao-navegar-para-tabela-de-publicacoes">
+                        Tabela de Publicações
+                        <i class="icone-navegar bi bi-chevron-right"></i>
+                    </button>
+                </li>
+            </ul>
+        </nav>  
+        <div id="container-titulo-de-fundo">
+            <h1 class="titulo-de-fundo">DEEPGROOVE</h1>
+        </div>
+        <footer id="rodape">
+            <button class="botao botao-home">
+                <i class="icone-home bi bi-house-door-fill"></i>
+            </button>
+            <button class="botao botao-sign-out">
+                <i class="icone-sign-out bi bi-box-arrow-right"></i>
+            </button>
+        </footer>
+        <div class="container-imagem-disco-de-vinil-fundo">
+            <img class="imagem-disco-de-vinil-fundo" src="../../../public/assets/disco-de-vinil-cortado-ao-meio.png" alt="Disco de vinil no fundo">
+        </div>
+    </main>
+    <script src="../../../public/js/dashboard.js"></script>
+</body>
+</html>
---
+++ app/views/site/login.view.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../public/css/login-e-cadastro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="container-login">
+        <div class="metade-tela-login formulario">
+            <div class="container-titulo">
+                <div class="container-seta">
+                    <i class="bi bi-arrow-left-circle-fill seta"></i>
+                </div>
+                <div>
+                     <h1 class="titulo-login">Entrar</h1>           
+                </div>
+            </div>
+            <form class="container-form">
+                <div class="container-inputs"></div>
+                    <div class="container-elemento-input">
+                        <h2>Email:</h2>
+                        <input placeholder="Digite seu email">
+                    </div>
+                    <div class="container-elemento-input">
+                        <h2>Senha:</h2>
+                        <input placeholder="Digite sua senha">
+                    </div>
+                    <div class="container-botao">
+                        <button class="botao-login">Entrar</button>
+                        <h3>Não possui uma conta? <button class="texto-sublinhado" type="button" style="color: #F09859; text-decoration: underline;" onclick="moverImagem()">Cadastre-se</button></h3>  
+                    </div>
+            </form>
+            <div class="container-logo">
+                <img class="logo-login" src="../../../public/assets/LogoTextoIcone (1).png">
+            </div>
+        </div>
+        <div class="metade-tela-login formulario">
+            <div class="container-titulo">
+                <div class="container-seta">
+                    <i class="bi bi-arrow-left-circle-fill seta"></i>
+                </div>
+                <div>
+                    <h1 class="titulo-login">Cadastro</h1>
+                </div>
+            </div>
+            <form class="container-form">
+                <div class="container-inputs"></div>
+                    <div class="container-elemento-input">
+                        <h2>Nome:</h2>
+                        <input placeholder="Digite seu nome">
+                    </div>
+                    <div class="container-elemento-input">
+                        <h2>Email:</h2>
+                        <input placeholder="Digite seu email">
+                    </div>
+                    <div class="container-elemento-input">
+                        <h2>Senha:</h2>
+                        <input placeholder="Digite sua senha">
+                    </div>
+                    <div class="container-botao">
+                        <button class="botao-login">Cadastrar</button>
+                        <h3>Já possui uma conta? <button class="texto-sublinhado" type="button" style="color: #F09859; text-decoration: underline;" onclick="moverImagem()">Faça login</button></h3>  
+                    </div>
+            </form>
+            <div class="container-logo">
+                <img class="logo-login" src="../../../public/assets/LogoTextoIcone (1).png">
+            </div>
+        </div>
+        <div class="imagem-cadastro" id="imagem-login-cadastro">
+        </div>        
+    </div>
+    <div class="login-mobile">
+        <div class="imagem-mobile"></div>
+        <div class="inputs-mobile">
+            <div class="titulo-mobile">
+                <h1 class="titulo-login mobile">Entrar</h1>
+            </div>
+            <form class="container-form-mobile">
+                <div class="container-inputs-mobile">
+                    <div class="container-elemento-input-mobile">
+                        <h2>Email:</h2>
+                        <input placeholder="Digite seu email">
+                    </div>
+                    <div class="container-elemento-input-mobile">
+                        <h2>Senha:</h2>
+                        <input placeholder="Digite sua senha">
+                    </div>
+                </div>
+                <div class="container-botao">
+                    <button class="botao-login">Entre</button>
+                    <h3>Não possui uma conta? <button class="texto-sublinhado" type="button" style="color: #F09859; text-decoration: underline;" onclick="moverImagem()">Cadastre-se</button></h3>            
+                </div>
+            </form>
+        </div>
+    </div>  
+    <script src="../../../public/js/login-e-cadastro.js"></script>
+</body>
+</html>
