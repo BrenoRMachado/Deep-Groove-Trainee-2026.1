f75dead773845277651be78fc8b882b21b613929
Luan Dutra <ldasilvadutradias@gmail.com>
2026-06-17 03:07:18 -0300

    feat(dashboard): view recebe acesso aos 3 ultimos posts publicados e sua data formatada

--- app/Controllers/dashboardController.php
+++ app/Controllers/dashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Core\App;
 use Exception;
+use DateTime;
 
 // !composer dump-autoload reconstrói as rotas
 
@@ -12,13 +13,22 @@ class dashboardController {
 
         $bancoDeDados = App::get('database');
 
-        $totalDePosts = $bancoDeDados -> countAll('publicacoes');
-
         $totalDeUsuarios = $bancoDeDados -> countAll('usuarios');
 
+        $totalDePosts = $bancoDeDados -> countAll('publicacoes');
+
+        $publicacoesRecentes = $bancoDeDados -> selecionaUltimos3PostsPublicados();
+
+        foreach($publicacoesRecentes as $publicacao){
+            $data = new DateTime($publicacao -> data_da_publicacao);
+            $data -> modify('-3 hours');
+            $publicacao -> data_da_publicacao = $data -> format('d/m/Y H:i');
+        }
+
         return view('admin/dashboard', [
+            'totalDeUsuarios' => $totalDeUsuarios,
             'totalDePosts' => $totalDePosts,
-            'totalDeUsuarios' => $totalDeUsuarios
+            'publicacoesRecentes' => $publicacoesRecentes
         ]);
 
     }
