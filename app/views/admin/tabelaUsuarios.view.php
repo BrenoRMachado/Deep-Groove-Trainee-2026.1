16fec196b75888aaa1d76193f6a587aae8f42e0d
Luan Dutra <ldasilvadutradias@gmail.com>
2026-06-18 11:46:59 -0300

    feat(tabelaUsuarios): botao toggle agora so aparece caso a pessoa logada seja administradora

--- app/views/admin/tabelaUsuarios.view.php
+++ app/views/admin/tabelaUsuarios.view.php
@@ -75,22 +75,24 @@
                 <div class="quarto-da-area-informacoes"><div class="container-dado-a-criar" style="background-color: var(--cor-vinho-100);">Nome</div> <input placeholder="Digite o nome" type="text" name="nome" required></div>
                 <div class="quarto-da-area-informacoes"><div class="container-dado-a-criar" style="background-color: var(--cor-laranja-200);">Email</div> <input placeholder="Digite o email" type="email" name="email" required></div>
                 <div class="quarto-da-area-informacoes"><div class="container-dado-a-criar" style="background-color: var(--cor-vermelho-50);">Senha</div> <input class="criar-senha" placeholder="Digite a senha" type="password" name="senha" required> <button class="botao-de-visualizar-senha-no-modal-de-criar" type="button" onclick="alternarVisualizacaoDaSenhaAoCriarUsuario()" > <i class="icone-visualizar-senha-no-modal-de-criar bi bi-eye-slash-fill"></i> </button> </div>
-                <div class="quarto-da-area-informacoes">
-                    <div class="container-dado-a-criar" style="background-color: var(--cor-laranja-100);">
-                        Admin
-                    </div> 
-                    <button type="button" class="botao-toggle pin-inativo" id="botao-toggle" onclick="acionarBotaoToggle('#botao-toggle', '#texto-permissao-admin', '#is-admin-input')">
-                        <span class="texto-permissao-admin" id="texto-permissao-admin">
-                            Acesso padrão
-                        </span>
-                        <span class="fundo-toggle">
-                            <span class="pin-toggle">
-    
+                <?php if($_SESSION['is_admin']): ?>
+                    <div class="quarto-da-area-informacoes">
+                        <div class="container-dado-a-criar" style="background-color: var(--cor-laranja-100);">
+                            Admin
+                        </div> 
+                        <button type="button" class="botao-toggle pin-inativo" id="botao-toggle" onclick="acionarBotaoToggle('#botao-toggle', '#texto-permissao-admin', '#is-admin-input')">
+                            <span class="texto-permissao-admin" id="texto-permissao-admin">
+                                Acesso padrão
                             </span>
-                        </span>
-                    </button>
-                    <input type="hidden" name="is_admin" id="is-admin-input" value="0">
-                </div>
+                            <span class="fundo-toggle">
+                                <span class="pin-toggle">
+        
+                                </span>
+                            </span>
+                        </button>
+                        <input type="hidden" name="is_admin" id="is-admin-input" value="0">
+                    </div>
+                <?php endif; ?>
                 <div class="quarto-da-area-informacoes">
                     <button class="botao-modal-criar cancelar cresce" type="reset" onclick="fecharModal('#modal-criar-usuarios')">Cancelar</button>
                     <button class="botao-modal-criar salva cresce" type="submit">Salvar</button>
@@ -380,20 +382,22 @@
                     <span class="titulo-dado titulo-senha">Senha</span>
                     <input class="editar-dado editar-senha" type="password" placeholder="Nova senha" name="senha">
                 </div>
-                <div class="container-editar-dado container-editar-admin">
-                    <span class="titulo-dado titulo-admin">Admin</span>
-                    <button type="button" class="botao-toggle pin-inativo" id="botao-toggle<?= $usuario -> id ?>" onclick="acionarBotaoToggle('#botao-toggle<?= $usuario -> id ?>', '#texto-permissao-admin<?= $usuario -> id ?>', '#is-admin-input<?= $usuario -> id ?>')">
-                        <span class="texto-permissao-admin" id="texto-permissao-admin<?= $usuario -> id ?>">Acesso padrão</span>
-                        <span class="fundo-toggle">
-                            <span class="pin-toggle"></span>
-                        </span>
-                    </button>
-                    <input type="hidden" name="is_admin" id="is-admin-input<?= $usuario -> id ?>" value="0">
-                </div>
-                <div class="secao-botoes-salvar-cancelar">
-                    <button class="botao botao-cancelar" type="reset" onclick="fecharModal('#modal-edicao-usuarios<?= $usuario->id ?>')">Cancelar</button>
-                    <button class="botao botao-salvar" type="submit">Salvar</button>
-                </div>
+                <?php if($_SESSION['is_admin']): ?>
+                    <div class="container-editar-dado container-editar-admin">
+                        <span class="titulo-dado titulo-admin">Admin</span>
+                        <button type="button" class="botao-toggle pin-inativo" id="botao-toggle<?= $usuario -> id ?>" onclick="acionarBotaoToggle('#botao-toggle<?= $usuario -> id ?>', '#texto-permissao-admin<?= $usuario -> id ?>', '#is-admin-input<?= $usuario -> id ?>')">
+                            <span class="texto-permissao-admin" id="texto-permissao-admin<?= $usuario -> id ?>">Acesso padrão</span>
+                            <span class="fundo-toggle">
+                                <span class="pin-toggle"></span>
+                            </span>
+                        </button>
+                        <input type="hidden" name="is_admin" id="is-admin-input<?= $usuario -> id ?>" value="0">
+                    </div>
+                    <div class="secao-botoes-salvar-cancelar">
+                        <button class="botao botao-cancelar" type="reset" onclick="fecharModal('#modal-edicao-usuarios<?= $usuario->id ?>')">Cancelar</button>
+                        <button class="botao botao-salvar" type="submit">Salvar</button>
+                    </div>
+                <?php endif; ?>
             </section>
         </form>
     </div>
