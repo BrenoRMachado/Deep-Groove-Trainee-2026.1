338677cec82b1301604df5b3d5509f97303d6a47
Laura <lauraobastoos@gmail.com>
2026-06-21 16:58:32 -0300

    fix(sidebar): conclusão da responsividade e funcionamento

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,35 +1,8 @@
 // *Seleciona ícones das páginas administrativas da sidebar e botão de abrir e fechá-la
-const nomeIconesPaginasAdministrativasSidebar = document.querySelectorAll('.nome-pg-adm');
-// const botaoAbrirFecharSidebar = document.querySelector('.botao-abrir-fechar-sidebar');
-// const logoSidebar = document.querySelector('.logo-principal-deep-groove');
-const intensNavegacao = document.getElementById("icones-admin");
-const sidebar = document.getElementById("sidebarjs");
+    const hamburger = document.getElementById("btn-hamburger");
+    const sidebar = document.getElementById("sidebarjs");
 
-let sidebar = 'fechado';
-
-// *Evento de abrir e fechar a sidebar ao clicar no botão
-botaoAbrirFecharSidebar.addEventListener('click', function(){
-    if (sidebar === 'fechado'){
-        nomeIconesPaginasAdministrativasSidebar.forEach((nomeIconesPaginasAdministrativasSidebar) => {
-            nomeIconesPaginasAdministrativasSidebar.classList.remove('nome-pg-adm');
-        });
-        sidebar = 'aberto';
-
-        // sidebar.style.width = "fit-content";
-        // intensNavegacao.style.alignItems = "center";
-
-        // botaoAbrirFecharSidebar.classList.remove('ativo');
-
-    }
-    else if (sidebar === 'aberto'){
-        nomeIconesPaginasAdministrativasSidebar.forEach((nomeIconesPaginasAdministrativasSidebar) => {
-            nomeIconesPaginasAdministrativasSidebar.classList.add('nome-pg-adm');
-        });
-        sidebar = 'fechado';
-
-        // sidebar.style.width = "fit-content";
-        // intensNavegacao.style.alignItems = "center";
-
-        // botaoAbrirFecharSidebar.classList.add('ativo');
-    }
-});
+    // Abre o menu 
+    hamburger.addEventListener("click", () => {
+        sidebar.classList.toggle("active");
+    });
